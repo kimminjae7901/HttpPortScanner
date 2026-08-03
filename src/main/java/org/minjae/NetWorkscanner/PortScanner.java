69ea84be7752471d4d84d1438470103db6da8486
kimminjae7901 <kimminjae7684@naver.com>
2026-08-04 01:27:14 +0900

    서비스 이름 출력 추가

--- src/main/java/org/minjae/NetWorkscanner/PortScanner.java
+++ src/main/java/org/minjae/NetWorkscanner/PortScanner.java
@@ -21,8 +21,53 @@ class ScanTask implements Runnable {
     }
 }
 
+
 public class PortScanner
 {
+    public static String getServiceName(int port){
+    switch (port)
+    {
+        case 20:
+        case 21:
+            return "FTP";
+
+        case 22:
+            return "SSH";
+
+        case 23:
+            return "TELNET";
+
+        case 25:
+            return "SMTP";
+
+        case 53:
+            return "DNS";
+
+        case 80:
+            return "HTTP";
+
+        case 110:
+            return "POP3";
+
+        case 143:
+            return "IMAP";
+
+        case 443:
+            return "HTTPS";
+
+        case 3306:
+            return "MySQL";
+
+        case 3389:
+            return "RDP";
+
+        case 8080:
+            return "HTTP-ALT";
+
+        default:
+            return "Unknown";
+    }
+    }
     public static void main(String[] args) {
 
         Scanner scanner=new Scanner(System.in);
@@ -55,7 +100,7 @@ public class PortScanner
         try{
             Socket socket=new Socket();
             socket.connect(new InetSocketAddress(ip,port),500);
-            System.out.println("[OPEN]"+port);
+            System.out.println("[OPEN]"+port+" "+getServiceName(port));
 
             socket.close();
         }
