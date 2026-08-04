666e24b6fc8c8e4f61f0c1954371c2d0b3b70c51
kimminjae7901 <kimminjae7684@naver.com>
2026-08-04 18:43:25 +0900

    http도메인 입력 추가

--- src/main/java/org/minjae/httpClient/httpClient.java
+++ src/main/java/org/minjae/httpClient/httpClient.java
@@ -1,22 +1,27 @@
-package org.minjae.NetWorkscanner;
+package org.minjae.httpClient;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class httpClient {
 
     public static void main(String[] args) {
 
         try {
-            Socket socket = new Socket("example.com", 80);
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("도메인을 입력하시오: ");
+            String host=scanner.nextLine();
+
+            Socket socket = new Socket(host, 80);
 
             OutputStream out = socket.getOutputStream();
-
+            
             String request =
                     "GET / HTTP/1.1\r\n" +
-                    "Host: example.com\r\n" +
+                    "Host: " +host+ "\r\n" +
                     "\r\n";
 
             out.write(request.getBytes());
